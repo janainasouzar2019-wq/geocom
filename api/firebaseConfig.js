be2a47e4618681297863d877a15085b813d9fc27
janainasouzar2019-wq <janaina.souzar2019@gmail.com>
2025-09-16 23:23:56 -0300

    Update firebaseConfig.js

--- api/firebaseConfig.js
+++ api/firebaseConfig.js
@@ -1,12 +1,12 @@
 // Acessa as variáveis de ambiente que o Vercel injeta neste ambiente de servidor.
 // O "process.env" é um objeto do Node.js que só funciona aqui no backend.
 const firebaseConfig = {
-  apiKey: "AIzaSyDIKWVZ1UfNSnucKnuhuqdo24FEz8g4MiM",
-  authDomain: "teste-final-4978a.firebaseapp.com",
-  projectId: "teste-final-4978a",
-  storageBucket: "teste-final-4978a.firebasestorage.app",
-  messagingSenderId: "601648580727",
-  appId: "1:601648580727:web:23000458a525ec71861e72"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 // Esta função retorna a configuração do Firebase
 module.exports = (req, res) => {
